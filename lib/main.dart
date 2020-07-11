8eec4dd5cbeacfecebf72cd94c28fb36204bdd38
Murilo Beltrame <murilobeltrame@gmail.com>
2020-07-11 10:10:22 -0300

    update interface

--- lib/main.dart
+++ lib/main.dart
@@ -3,47 +3,62 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: 'Contador de Pessoas',
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text(
-            "Pessoas: 0",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold
-            )
+          Image.asset(
+            'images/restaurant.jpg',
+            fit: BoxFit.cover,
+            height: 1000.0,
           ),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlatButton(
-                child: Text(
-                  "+1",
+              Text(
+                  "Pessoas: 0",
                   style: TextStyle(
                       color: Colors.white,
-                      fontSize: 40.0
-                  ),
-                ),
-                onPressed: (){},
+                      fontWeight: FontWeight.bold
+                  )
               ),
-              FlatButton(
-                child: Text(
-                  "-1",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40.0
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0
+                        ),
+                      ),
+                      onPressed: (){},
+                    ),
                   ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0
+                        ),
+                      ),
+                      onPressed: (){},
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode entrar",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
                 ),
-                onPressed: (){},
               )
             ],
-          ),
-          Text(
-            "Pode entrar",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold
-            ),
           )
         ],
       )
