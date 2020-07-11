dbfef51f1e6f713e648ff21533fc5f82655810d6
Murilo Beltrame <murilobeltrame@gmail.com>
2020-07-11 00:50:31 -0300

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'Contador de Pessoas',
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0
+                  ),
+                ),
+                onPressed: (){},
+              ),
+              FlatButton(
+                child: Text(
+                  "-1",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0
+                  ),
+                ),
+                onPressed: (){},
+              )
+            ],
+          ),
+          Text(
+            "Pode entrar",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            ),
+          )
+        ],
+      )
+  ));
+}
